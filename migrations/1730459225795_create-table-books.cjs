99f42d480c5dc523af9fa6e6381693bc8db6f917
Bayu <bayutiadi04@gmail.com>
2024-11-01 18:09:41 +0700

    added books table

---
+++ migrations/1730459225795_create-table-books.cjs
@@ -0,0 +1,71 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable("books", {
+    id: {
+      type: "VARCHAR(50)",
+      primaryKey: true,
+    },
+    title: {
+      type: "TEXT",
+      notNull: true,
+    },
+    published: {
+      type: "TEXT",
+      notNull: true,
+    },
+    author: {
+      type: "TEXT",
+      notNull: true,
+    },
+    genre: {
+      type: "TEXT",
+      notNull: true,
+    },
+    format: {
+      type: "TEXT",
+      notNull: true,
+    },
+    isbn: {
+      type: "TEXT",
+      notNull: true,
+    },
+    description: {
+      type: "TEXT",
+      notNull: true,
+    },
+    bookURL: {
+      type: "TEXT",
+      // notNull: true,
+    },
+    book_image_url: {
+      type: "TEXT",
+      // notNull: true,
+    },
+    ratingtotal: {
+      type: "NUMERIC",
+      default: 0,
+    },
+    ratingcount: {
+      type: "NUMERIC",
+      default: 0,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable("books");
+};
